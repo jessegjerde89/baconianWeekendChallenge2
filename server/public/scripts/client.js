ee5af434bf4de9bd73be3992f68476a638c269d9
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-04-22 09:34:44 -0500

    basic interface and Clear working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,23 @@
 $( document ).ready( onReady );
 
+function clearInputs(){
+    $( '#num0In' ).val( '' );
+    $( '#num1In' ).val( '' );
+}
+
+function getHistory(){
+    // AJAX GET call to /history
+    $.ajax({
+        type: 'GET',
+        url: '/history'
+    }).then( function( response ){
+        console.log( 'back from GET /history with:', response );
+        // loop through response
+        // display each on the page
+    })
+}
+
 function onReady(){
-    console.log( 'JQ' );
+    getHistory();
+    $( '#clearInputsButton' ).on( 'click', clearInputs );
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,20 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // globals
 const port = 5000;
+let history = [];
 
 // spin up server
 app.listen( port, ()=>{
     console.log( 'server is up on:', port );
 }); // end server up
+
+// routes
+app.post( '/calculate', ( req, res ) =>{
+    console.log( '/calculate POST hit:', req.body );
+    res.send( 'meow' );
+}) // end /calculate POST
+
+app.get( '/history', ( req, res )=>{
+    console.log( '/history GET hit' );
+    res.send( history );
+}) // end /history GET
